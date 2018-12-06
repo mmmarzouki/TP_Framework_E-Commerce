0ad30d8aa8a1a40c5dbea4cafaa0820b6278140d
MedMelek <mmmarzouki@gmail.com>
2018-12-06 10:31:15 -0800

    added OrderBook

--- DAL/dal/src/main/java/insat/ecommerce/dal/model/OrderBook.java
+++ DAL/dal/src/main/java/insat/ecommerce/dal/model/OrderBook.java
@@ -1,7 +1,66 @@
 package insat.ecommerce.dal.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
+@Entity(name="orderBooks")
 public class OrderBook {
 
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+
+    @ManyToMany(mappedBy="orderBooks")
+    private Cart cart;
+
+    @ManyToMany(mappedBy="orderBooks")
+    private Book book;
+
+    public OrderBook() {
+    }
+
+    public OrderBook(int id, Cart cart, Book book, int number) {
+        this.id = id;
+        this.cart = cart;
+        this.book = book;
+        this.number = number;
+    }
+    private int number;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cart getCart() {
+        return this.cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Book getBook() {
+        return this.book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
 }
