b87362a4d695bb0ca4ea5bf89fed0d8745867760
MedMelek <mmmarzouki@gmail.com>
2018-12-06 12:08:14 -0800

    added CRUD methods BookController

--- DAL/dal/src/main/java/insat/ecommerce/dal/controller/BookController.java
+++ DAL/dal/src/main/java/insat/ecommerce/dal/controller/BookController.java
@@ -8,21 +8,49 @@ import org.springframework.web.bind.annotation.RestController;
 import insat.ecommerce.dal.model.Book;
 import insat.ecommerce.dal.repository.BookRepository;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class BookController {
 
+    //repositories
     @Autowired
     BookRepository bookRepository;
 
-    @RequestMapping(value="path", method=RequestMethod.GET)
-    public List<Book> findAll(@RequestParam String param) {
+    //methods
+
+    //find all
+    @RequestMapping(value="/books", method=RequestMethod.GET)
+    public List<Book> findAll() {
         return (List<Book>) bookRepository.findAll();
     }
     
+    //find one
+    @RequestMapping(value="/books/{id}", method=RequestMethod.GET)
+    public Book findOne(@PathVariable int id) {
+        return bookRepository.findById(id).get();
+    }
     
+    //create
+    @RequestMapping(value="/books",method=RequestMethod.POST)
+    public void create(@RequestBody Book book){
+        bookRepository.save(book);
+    }
+    
+    //update
+    @RequestMapping(value="/books",method=RequestMethod.PUT)
+    public void update(@RequestBody Book book){
+        bookRepository.save(book);
+    }
+
+    //delete
+    @RequestMapping(value="/books/{id}",method=RequestMethod.DELETE)
+    public void delete(@PathVariable int id){
+        bookRepository.deleteById(id);
+    }
+
 }
