e66b538e724b9b58d265e4932d8faa47bad98e6d
MedMelek <mmmarzouki@gmail.com>
2018-12-06 10:18:04 -0800

    added Client

--- DAL/dal/src/main/java/insat/ecommerce/dal/model/Client.java
+++ DAL/dal/src/main/java/insat/ecommerce/dal/model/Client.java
@@ -1,6 +1,119 @@
 package insat.ecommerce.dal.model;
 
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity(name="clients")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String email;
+
+    @Column
+    private String passwd;
+
+    @OneToOne(mappedBy="client")
+    private Cart cart;
+
+    @OneToMany(mappedBy="clients")
+    private ArrayList<Order> orders;
+
+    public Client() {
+    }
+
+    public Client(int id, String name, String lastName, String phoneNumber, String email, String passwd, Cart cart) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.passwd = passwd;
+        this.cart = cart;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswd() {
+        return this.passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public Cart getCart() {
+        return this.cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public ArrayList<Order> getOrders() {
+        return this.orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
 
     
 }
